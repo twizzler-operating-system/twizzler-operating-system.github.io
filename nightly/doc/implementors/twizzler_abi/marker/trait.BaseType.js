ce5e48bf7154785c90ab9ff5be1589a5453c6b97
Deploy Actions <null@null.com>
2023-03-10 17:05:55 +0000

    Update

--- nightly/doc/implementors/twizzler_abi/marker/trait.BaseType.js
+++ nightly/doc/implementors/twizzler_abi/marker/trait.BaseType.js
@@ -2,5 +2,5 @@
 "twizzler_abi":[],
 "twizzler_net":[["impl BaseType for <a class=\"struct\" href=\"twizzler_net/buffer/struct.BufferBase.html\" title=\"struct twizzler_net::buffer::BufferBase\">BufferBase</a>"]],
 "twizzler_object":[],
-"twizzler_queue":[["impl&lt;S, C&gt; BaseType for <a class=\"struct\" href=\"twizzler_queue/struct.QueueBase.html\" title=\"struct twizzler_queue::QueueBase\">QueueBase</a>&lt;S, C&gt;"]]
+"twizzler_queue_raw":[["impl&lt;S, C&gt; BaseType for <a class=\"struct\" href=\"twizzler_queue_raw/struct.QueueBase.html\" title=\"struct twizzler_queue_raw::QueueBase\">QueueBase</a>&lt;S, C&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ nightly/doc/twizzler_abi/pager/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":[["KernelCommand",""],["KernelCompletionData",""]],"struct":[["CompletionToKernel",""],["RequestFromKernel",""]]};
