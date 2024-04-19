0d01fa4ea00f06e14c076457772ea365dd7d4179
Deploy Actions <null@null.com>
2024-04-19 21:07:54 +0000

    Update

--- nightly/doc/crates.js
+++ nightly/doc/crates.js
@@ -1,1 +1,1 @@
-window.ALL_CRATES = ["dynlink","monitor","monitor_api","nvme","secgate","twizzler_abi","twizzler_async","twizzler_driver","twizzler_net","twizzler_object","twizzler_queue","twizzler_queue_raw","twizzler_runtime_api","twz_rt"];
+window.ALL_CRATES = ["dynlink","monitor","monitor_api","nvme","secgate","twizzler_abi","twizzler_async","twizzler_driver","twizzler_futures","twizzler_net","twizzler_object","twizzler_queue","twizzler_queue_raw","twizzler_runtime_api","twz_rt"];
--- nightly/doc/src-files.js
+++ nightly/doc/src-files.js
@@ -7,6 +7,7 @@ var srcIndex = JSON.parse('{\
 "twizzler_abi":["",[["arch",[["x86_64",[],["mod.rs","syscall.rs","upcall.rs"]]],["mod.rs"]],["device",[["bus",[],["mod.rs","pcie.rs"]]],["mod.rs"]],["syscall",[["time",[],["clock.rs","mod.rs","timedefs.rs","units.rs"]]],["console.rs","create.rs","handle.rs","info.rs","kaction.rs","map.rs","mod.rs","object_control.rs","object_stat.rs","security.rs","spawn.rs","thread_control.rs","thread_sync.rs"]],["thread",[],["event.rs"]]],["aux.rs","kso.rs","lib.rs","marker.rs","meta.rs","object.rs","pager.rs","security.rs","slot.rs","thread.rs","upcall.rs"]],\
 "twizzler_async":["",[],["async_source.rs","block_on.rs","event.rs","exec.rs","future.rs","lib.rs","reactor.rs","run.rs","task.rs","thread_local.rs","throttle.rs","timer.rs"]],\
 "twizzler_driver":["",[["arch",[],["mod.rs","x86.rs"]],["bus",[],["mod.rs","pcie.rs"]],["device",[],["children.rs","events.rs","info.rs","mmio.rs","mod.rs"]],["dma",[],["mod.rs","object.rs","pin.rs","pool.rs","region.rs"]],["request",[],["async_ids.rs","inflight.rs","mod.rs","requester.rs","response_info.rs","submit.rs","summary.rs"]]],["controller.rs","lib.rs"]],\
+"twizzler_futures":["",[],["lib.rs"]],\
 "twizzler_net":["",[],["addr.rs","buffer.rs","lib.rs","nm_handle.rs","req.rs","rx_req.rs","tx_req.rs"]],\
 "twizzler_object":["",[],["base.rs","create.rs","init.rs","lib.rs","marker.rs","meta.rs","object.rs","ptr.rs","slot.rs"]],\
 "twizzler_queue":["",[],["callback_queue.rs","lib.rs","queue.rs","sender_queue.rs"]],\
