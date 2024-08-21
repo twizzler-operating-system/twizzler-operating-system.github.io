101cb011a8820a7d0b8765f3298fd481c5af97b5
Deploy Actions <null@null.com>
2024-08-21 00:17:14 +0000

    Update

--- nightly/doc/secgate/sidebar-items.js
+++ nightly/doc/secgate/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"attr":["secure_gate"],"constant":["SECGATE_TRAMPOLINE_ALIGN"],"enum":["SecGateReturn"],"macro":["secgate_prelude"],"struct":["Arguments","GateCallInfo","Return","SecGateInfo"],"trait":["Crossing"],"type":["RawSecGateInfo"]};
+window.SIDEBAR_ITEMS = {"attr":["secure_gate"],"constant":["SECGATE_TRAMPOLINE_ALIGN"],"enum":["SecGateReturn"],"macro":["secgate_prelude"],"mod":["util"],"struct":["Arguments","GateCallInfo","Return","SecGateInfo"],"trait":["Crossing"],"type":["RawSecGateInfo"]};
---
+++ nightly/doc/secgate/util/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["HandleMgr","SimpleBuffer"],"trait":["Handle"],"type":["Descriptor"]};
