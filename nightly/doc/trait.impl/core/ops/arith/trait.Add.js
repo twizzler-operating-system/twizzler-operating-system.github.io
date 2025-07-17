e4b4e9d4c5e805b3eb371f7384e01b93237e04b2
Deploy Actions <null@null.com>
2025-07-17 03:52:35 +0000

    Update

---
+++ nightly/doc/trait.impl/core/ops/arith/trait.Add.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["twizzler_abi",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Add.html\" title=\"trait core::ops::arith::Add\">Add</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u64.html\">u64</a>&gt; for <a class=\"struct\" href=\"twizzler_abi/pager/struct.PhysRange.html\" title=\"struct twizzler_abi::pager::PhysRange\">PhysRange</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[405]}
